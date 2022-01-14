93b6d140594a8f0117d874bc2c82e9144b5db700
feizheng <1290657123@qq.com>
2022-01-14 16:32:15 +0800

    feat: add nx structure

--- jsw_nx/__init__.py
+++ jsw_nx/__init__.py
@@ -1,3 +1,4 @@
 __version__ = '0.1.0'
 from jsw_nx.base.set import set
+from jsw_nx.base.forin import forin
 from jsw_nx.base.get import get
--- jsw_nx/base/set.py
+++ jsw_nx/base/set.py
@@ -1,2 +1,7 @@
 def set():
     print('set nx.')
+
+# each with index
+def each(arr, fn):
+    for i in range(len(arr)):
+        fn(arr[i], i)
