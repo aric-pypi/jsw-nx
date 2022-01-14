edee666719b0740a5d29eaab899269304881f3b7
feizheng <1290657123@qq.com>
2022-01-14 17:14:34 +0800

    feat: new publish

--- jsw_nx/__init__.py
+++ jsw_nx/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.0'
+__version__ = '1.0.1'
 from jsw_nx.base.set import set
 from jsw_nx.base.forin import forin
 from jsw_nx.base.get import get
--- tests/test_jsw_nx.py
+++ tests/test_jsw_nx.py
@@ -1,9 +1,4 @@
 import jsw_nx
-from jsw_nx import __version__
-
-
-def test_version():
-    assert __version__ == '0.1.0'
 
 def test_packages():
     assert jsw_nx.days(2021,1) == 31
