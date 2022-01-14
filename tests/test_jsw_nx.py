f8a3282cf9a0e51f3f5e9e9f87de75751d206730
feizheng <1290657123@qq.com>
2022-01-14 17:02:33 +0800

    chore: add more placehodler

--- tests/test_jsw_nx.py
+++ tests/test_jsw_nx.py
@@ -1,5 +1,11 @@
+import jsw_nx
 from jsw_nx import __version__
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_packages():
+    assert jsw_nx.days(2021,1) == 31
+    assert jsw_nx.days(2021,2) == 28
+    assert jsw_nx.days(2021,4) == 30
