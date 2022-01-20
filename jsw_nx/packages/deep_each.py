e1d04c9ea4858216a4773a4be816594532ff3c93
feizheng <1290657123@qq.com>
2022-01-20 22:23:16 +0800

    fix: deep-each

--- jsw_nx/packages/deep_each.py
+++ jsw_nx/packages/deep_each.py
@@ -1,15 +1,11 @@
 def deep_each(obj, func):
     def inner_each(key, value, parent, fn):
+        fn(key, value, parent)
         if isinstance(value, dict):
             for k, v in value.items():
                 inner_each(k, v, value, fn)
-        elif isinstance(value, list):
+        if isinstance(value, list):
             for i, v in enumerate(value):
                 inner_each(i, v, value, fn)
-        else:
-            fn(key, value, parent)
 
-    obj = [obj] if isinstance(obj, dict) else obj
-
-    for i, v in enumerate(obj):
-        inner_each(i, v, obj, func)
+    inner_each(None, obj, None, func)
