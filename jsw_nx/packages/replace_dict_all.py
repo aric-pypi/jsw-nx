2727e95b6b14d54ebd8fd7efec00d96026e7fbe6
feizheng <1290657123@qq.com>
2022-02-25 10:25:30 +0800

    feat: repalce_dict_all support re

--- jsw_nx/packages/replace_dict_all.py
+++ jsw_nx/packages/replace_dict_all.py
@@ -1,7 +1,10 @@
 # https://stackoverflow.com/questions/6116978/how-to-replace-multiple-substrings-of-a-string
+# https://blog.csdn.net/geerniya/article/details/78672002
+import re
+
 
 def replace_dict_all(text, dic):
     for key in dic:
-        val = dict[key]
-        text = text.replace(key, val)
+        val = dic[key]
+        text = re.sub(key, val, text)
     return text
