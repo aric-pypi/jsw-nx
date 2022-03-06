e14b7ab92b2d19fa8e9d7e6c62b55064424364c3
feizheng <1290657123@qq.com>
2022-03-06 19:59:32 +0800

    feat: use ruamel.yaml instead of default yaml

--- jsw_nx/classes/yaml.py
+++ jsw_nx/classes/yaml.py
@@ -1,13 +1,17 @@
 from ruamel import yaml
 
 
-# https://blog.csdn.net/BreezePython/article/details/108770195
-
 class Yaml:
     @classmethod
     def stringify(cls, data):
-        return yaml.load(data, Loader=yaml.RoundTripLoader)
+        return yaml.dump(
+            data,
+            allow_unicode=True,
+            default_flow_style=False,
+            line_break=True,
+            Dumper=yaml.RoundTripDumper
+        )
 
     @classmethod
     def parse(cls, data):
-        return yaml.safe_load(data)
+        return yaml.load(data, Loader=yaml.RoundTripLoader)
