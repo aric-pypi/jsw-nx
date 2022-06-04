0ba92292548e4430e55cc71342eb208902840c9e
feizheng <1290657123@qq.com>
2022-06-04 10:46:17 +0800

    wip

--- jsw_nx/packages/filesize.py
+++ jsw_nx/packages/filesize.py
@@ -10,7 +10,7 @@ def filesize(filename):
     kb = size / 1024
     mb = size / 1024 / 1024
 
-    kb_2 = round(kb, 2) + 'KB'
-    mb_2 = round(mb, 2) + 'MB'
+    kb_2 = str(round(kb, 2)) + 'KB'
+    mb_2 = str(round(mb, 2)) + 'MB'
 
     return size, kb_2, mb_2
